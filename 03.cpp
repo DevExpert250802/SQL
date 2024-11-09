ecac3bc4be7f561268d98454fdff6f17e606fd52
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-11-10 01:43:00 +0530

    Reverse Integer

--- 03.cpp
+++ 03.cpp
@@ -2,21 +2,44 @@
 // https://leetcode.com/problems/reverse-integer/description/
 
 
+// Code using 64-bit integer
+
 class Solution {
 public:
     int reverse(int x) {
-        long rev_num=0;
+        long rev_num = 0;
         long n = x;
-        if(x>=INT_MAX || x<=INT_MIN) return 0;
-        else{
-        while(n){
-             int lastdigit =n%10;
-             rev_num = (rev_num*10) + lastdigit;
-             n=n/10;
-            }
+
+        while (n) {
+            int last_digit = n % 10;
+            rev_num = rev_num * 10 + last_digit;
+            n /= 10;
         }
-        if(rev_num >= INT_MIN && rev_num <= INT_MAX)
+        if (rev_num < INT_MIN || rev_num > INT_MAX)
+        return 0;
+        return  int(rev_num);
+    }
+};
+
+
+
+
+
+// Code using 32-bit integer
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev_num = 0;
+        int n = x;
+
+        while (n) {
+            int last_digit = n % 10;
+            if( (rev_num < INT_MIN/10) || (rev_num > INT_MAX/10)) return 0;
+            rev_num = rev_num * 10 + last_digit;
+            n /= 10;
+        }
         return rev_num;
-        else return 0;
     }
 };

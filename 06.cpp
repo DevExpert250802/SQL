b6468432c89415501cffdbeb01af065fce6ee82e
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-11-10 12:27:57 +0530

    Print all Divisors of a number

--- 06.cpp
+++ 06.cpp
@@ -88,6 +88,22 @@ vector<int> printDivisors(int n) {
 
 
 
+vector<int> printDivisors(int n) {
+    vector<int> divisors;
+
+    for (int i = 1; i*i <= n; i++) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (n / i != i) {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+
+    sort(divisors.begin(), divisors.end());
+    return divisors;
+}
+
 
 
 // T.C. = O(sqrt(N))
